ac3be2d62a56d6f4402fc56f5eec235f17b561e9
jeffersonbalde <jeffersonbalde8@gmail.com>
2025-07-03 20:55:41 +0800

    RE

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\admin\TempImageController;
 use App\Http\Controllers\AuthenticationController;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::post("authenticate", [AuthenticationController::class, "authenticate"]);
